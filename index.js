d2d812f696a20b4075f33d62cd483cd00b771273
Xavier.L <xavierlee0329@gmail.com>
2025-08-29 17:24:45 +0800

    exe 3.15-3.18

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+
+
 
 app.use(cors());
 app.use(morgan("tiny"));
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 app.use(express.json());
 
 const Phonebook = require("./models/phonebook");
-
 
 app.get("/api/persons", (request, response) => {
   Phonebook.find({}).then((result) => {
@@ -23,45 +25,80 @@ app.get("/api/persons", (request, response) => {
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   Phonebook.findById(id).then((result) => {
-    response.json(result);
-  })
+    if (result) {
+      response.json(result);
+    } else {
+      response.status(404).end();
+    }
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
+  Phonebook.findByIdAndDelete(id).then(result => {
+    if(result){
+      response.status(204).send({message: 'person deleted'});
+    }else{
+      response.status(404).send({error: 'person not found'});
+    }
+  })
+  .catch(error => next(error))
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  
-  Phonebook.find({}).then((persons) => {if (persons.find((p) => p.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  } else {
-    const person = new Phonebook({
-      name: body.name,
-      number: body.number,
-    })
 
-    person.save().then((result) => {
-      response.json(result);
-    })
-  }})
+  Phonebook.find({}).then((persons) => {
+    if (persons.find((p) => p.name === body.name)) {
+      return response.status(400).json({
+        error: "name must be unique",
+      });
+    } else {
+      const person = new Phonebook({
+        name: body.name,
+        number: body.number,
+      });
+
+      person.save().then((result) => {
+        response.json(result);
+      });
+    }
+  });
 });
 
 app.get("/info", (request, response) => {
-  const size = persons.length;
+  Phonebook.find({}).then((result) => {
+    const size = result.length;
   const date = new Date();
   response.send(`<div>
     <p>Phonebook has info for ${size} people</p>
     <p>${date}</p>
     </div>`);
+  });
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id
+  const {name, number} = request.body
+  Phonebook.findById(id).then(result => {
+    if (result) {
+      result.name = name
+      result.number = number
+      result.save().then(updated => {
+        response.json(updated)
+      })
+    }else{
+      response.status(404).send({error: 'person not found'})
+    }
+  })
+  .catch(error => next(error))
+  
+})
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(errorHandler)
